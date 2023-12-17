fbf02ef4c39487cef2ddaab1cb76a5d689336040
gupavi <abhinav200002@gmail.com>
2023-12-18 02:21:01 +0530

    better solutions added with formatting

--- DSA-self/Graphs by Striver/DetectCycleDirectedGraph(gfg_medium).cpp
+++ DSA-self/Graphs by Striver/DetectCycleDirectedGraph(gfg_medium).cpp
@@ -1,25 +1,28 @@
-//another way of checking cycle is to keep 2 arrays of size V
-// 1. visited which tracks what all nodes are visited atlease once - exactly the purpose of my soln.s isNotCyclic
-// 2. dfsVisited which tracks what all nodes are visited in a single directed traversal run - exactly same as visited array of my soln.
-// striver's video on directed graph cycle detection
-// https://www.youtube.com/watch?v=uzVUw90ZFIg&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=11
+// another way of checking cycle is to keep 2 arrays of size V
+//  1. visited which tracks what all nodes are visited atlease once - exactly the purpose of my soln.s isNotCyclic
+//  2. dfsVisited which tracks what all nodes are visited in a single directed traversal run - exactly same as visited array of my soln.
+//  striver's video on directed graph cycle detection
+//  https://www.youtube.com/watch?v=uzVUw90ZFIg&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=11
 
 // Solution 1
-class Solution {
-  public:
-    bool hasCycle(vector<int> adj[], int index, vector<bool> &visited,  vector<bool> &isNotCyclic){
-        
+class Solution
+{
+public:
+    bool hasCycle(vector<int> adj[], int index, vector<bool> &visited, vector<bool> &isNotCyclic)
+    {
+
         visited[index] = true;
-        for(int i=0; i<adj[index].size(); i++){
-            
-            int child = adj[index][i]; 
-            if(isNotCyclic[child])
-            continue;
-            
-            if(visited[child])
+        for (int i = 0; i < adj[index].size(); i++)
+        {
+
+            int child = adj[index][i];
+            if (isNotCyclic[child])
+                continue;
+
+            if (visited[child])
                 return true;
-            
-            if(hasCycle(adj, child, visited, isNotCyclic))
+
+            if (hasCycle(adj, child, visited, isNotCyclic))
                 return true;
         }
         isNotCyclic[index] = true;
@@ -27,46 +30,54 @@ class Solution {
         return false;
     }
     // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
-        
-        vector<bool> visited(V, false); //visit tracking for a directed path
-        vector<bool> isNotCyclic(V, false); //keeps track of nodes in a component so that we don't run hasCycle again and again in the same component
-        
-        //components in graph will also get handled implictly this way
-        //this loop is necessary as all nodes may not get visited (in a component) starting from an index (say 0)
-        for(int i=0; i<V; i++){
-            if(!isNotCyclic[i] && hasCycle(adj, i, visited, isNotCyclic))
-            return true;
+    bool isCyclic(int V, vector<int> adj[])
+    {
+
+        vector<bool> visited(V, false);     // visit tracking for a directed path
+        vector<bool> isNotCyclic(V, false); // keeps track of nodes in a component so that we don't run hasCycle again and again in the same component
+
+        // components in graph will also get handled implictly this way
+        // this loop is necessary as all nodes may not get visited (in a component) starting from an index (say 0)
+        for (int i = 0; i < V; i++)
+        {
+            if (!isNotCyclic[i] && hasCycle(adj, i, visited, isNotCyclic))
+                return true;
         }
         return false;
     }
 };
-// time complexity = O(V+E) = overall loop over vertices run and on edges - 
-    // vertices on line 28 : to check if they are visited or not and then to run dfs on the,
-    // edges in total all are visited after exhaustive run on graph via isCyclicComponent function
+// time complexity = O(V+E) = overall loop over vertices run and on edges -
+// vertices on line 28 : to check if they are visited or not and then to run dfs on the,
+// edges in total all are visited after exhaustive run on graph via isCyclicComponent function
 // space = O(V)
 
 // Solution 2
-class Solution {
-    bool isCycle(vector<int> adj[], int index, vector<int> &visited){
-        if(visited[index] == 1)
+class Solution
+{
+    bool isCycle(vector<int> adj[], int index, vector<int> &visited)
+    {
+        if (visited[index] == 1)
             return true;
         visited[index] = 1;
-        for(int x: adj[index]){
-            if(visited[x] == -1)
+        for (int x : adj[index])
+        {
+            if (visited[x] == -1)
                 continue;
-            if(isCycle(adj, x, visited))
+            if (isCycle(adj, x, visited))
                 return true;
         }
         visited[index] = -1;
         return false;
     }
-  public:
+
+public:
     // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
+    bool isCyclic(int V, vector<int> adj[])
+    {
         vector<int> visited(V, 0);
-        for(int i=0; i<V; i++){
-            if(!visited[i] && isCycle(adj, i, visited))
+        for (int i = 0; i < V; i++)
+        {
+            if (!visited[i] && isCycle(adj, i, visited))
                 return true;
         }
         return false;
@@ -74,45 +85,91 @@ class Solution {
 };
 
 // Solution 3 - Kahn's Algo
-class Solution {
-  public:
+class Solution
+{
+public:
     // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
-        //usign BFS - Kahn's algo - topological sort BFS
-        
-        //will work for components in graph
-        
+    bool isCyclic(int V, vector<int> adj[])
+    {
+        // usign BFS - Kahn's algo - topological sort BFS
+
+        // will work for components in graph
+
         // O(E)
         vector<int> indegree(V, 0);
-        for(int i=0; i<V; i++){
-            for(int j=0; j<adj[i].size(); j++){
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < adj[i].size(); j++)
+            {
                 indegree[adj[i][j]]++;
             }
         }
-        
-        //O(V)
+
+        // O(V)
         queue<int> pending;
-        for(int i=0; i<V; i++){
-            if(indegree[i] == 0)
+        for (int i = 0; i < V; i++)
+        {
+            if (indegree[i] == 0)
                 pending.push(i);
         }
-        
+
         int cnt = 0;
-        
+
         // O(V + E) - not 2E since directed graph
-        while(!pending.empty()){
+        while (!pending.empty())
+        {
             int front = pending.front();
             pending.pop();
-            
-            for(int i=0; i<adj[front].size(); i++){
+
+            for (int i = 0; i < adj[front].size(); i++)
+            {
                 indegree[adj[front][i]]--;
-                if(indegree[adj[front][i]] == 0)
+                if (indegree[adj[front][i]] == 0)
                     pending.push(adj[front][i]);
             }
             cnt++;
         }
-        //T(n) = O(V + E)
-        //S(n) = O(V)
+        // T(n) = O(V + E)
+        // S(n) = O(V)
         return (cnt != V);
+    }
+};
+
+// Solution 4 - conventional way DFS
+class Solution
+{
+    bool isCycle(vector<int> adj[], int index, vector<bool> &visited, vector<bool> &pathVisited)
+    {
+
+        visited[index] = true;
+        pathVisited[index] = true;
+        for (int v : adj[index])
+        {
+            if (!visited[v])
+            {
+                if (isCycle(adj, v, visited, pathVisited))
+                    return true;
+            }
+            else if (pathVisited[v])
+                return true;
+        }
+        pathVisited[index] = false;
+        return false;
+    }
+
+public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[])
+    {
+
+        // conventional way for cycle detection in directed graph by DFS
+        vector<bool> visited(V, false);
+        vector<bool> pathVisited(V, false);
+        for (int i = 0; i < V; i++)
+        {
+            if (!visited[i] && isCycle(adj, i, visited, pathVisited))
+                return true;
+        }
+        return false;
     }
 };
--- DSA-self/Graphs by Striver/TopologicalSortDFS(gfg_medium).cpp
+++ DSA-self/Graphs by Striver/TopologicalSortDFS(gfg_medium).cpp
@@ -1,98 +1,195 @@
+#include <bits/stdc++.h>
+// Note: isCyclic should be checked before or during topoSort
+
 // Solution 1
 class Solution
 {
-    stack<int> ansStack;
-	public:
-	//Function to return list containing vertices in Topological order. 
-	void dfsApproach(vector<int> adj[], int index, vector<bool> &visited) {
-	    
-	    visited[index] = true;
-	    for(int i=0; i<adj[index].size(); i++){
-	        int child = adj[index][i];
-	        if(!visited[child])
-	            dfsApproach(adj, child, visited);
-	    }
-	    ansStack.push(index);
-	}
-	vector<int> topoSort(int V, vector<int> adj[]) 
+
+	bool isCyclic(vector<int> adj[], vector<int> &visited, int index, vector<int> &ans)
 	{
-	    vector<bool> visited(V, false); 
-	    for(int i=0; i<V; i++){
-	        if(!visited[i])    
-	            dfsApproach(adj, i, visited);
-	    }
-	    vector<int> ans;
-	    
-	    while(!ansStack.empty()){
-	        ans.push_back(ansStack.top());
-	        ansStack.pop();
-	    }
-	    return ans;
+
+		visited[index] = 1;
+		for (int v : adj[index])
+		{
+			if (visited[v] == 0)
+			{
+				if (isCyclic(adj, visited, v, ans))
+					return true;
+			}
+			else if (visited[v] == 1)
+				return true;
+		}
+		visited[index] = -1;
+		ans.push_back(index);
+		return false;
+	}
+
+public:
+	// Function to return list containing vertices in Topological order.
+	vector<int> topoSort(int V, vector<int> adj[])
+	{
+		// Topological sort DFS (inclusive of cycle detection (using single visited array))
+		vector<int> visited(V, 0);
+		vector<int> ans;
+		for (int i = 0; i < V; i++)
+		{
+			if (!visited[i] && isCyclic(adj, visited, i, ans))
+				return vector<int>();
+		}
+		reverse(ans.begin(), ans.end());
+		return ans;
 	}
 };
 
-// Solution 2
+// Solution 2 - conventional way
 class Solution
 {
-    // works for DAG only
-    bool isCycle(int index, stack<int> &stk, vector<int> adj[], vector<int> &visited){
-        visited[index] = 1;
-        for(int x: adj[index]){
-            if(visited[x] == -1)
-                continue;
-            if(visited[x] == 1 || isCycle(x, stk, adj, visited))
-                return true;
-        }
-        visited[index] = -1;
-        stk.push(index);
-        return false;
-    }
-	
-	public:
-	vector<int> topoSort(int V, vector<int> adj[]) 
+	bool isCyclic(vector<int> adj[], vector<bool> &visited, vector<bool> &pathVisited, int index, vector<int> &ans)
 	{
-	    stack<int> stk;
-	    vector<int> visited(V, 0);
-	    for(int i=0; i<V; i++){
-	        if(!visited[i] && isCycle(i, stk, adj, visited))
-	            return vector<int>();
-	    }
-	    vector<int> ans;
-	    while(!stk.empty()){
-	        ans.push_back(stk.top());
-	        stk.pop();
-	    }
-	    
-	    return ans;
+		visited[index] = true;
+		pathVisited[index] = true;
+
+		for (int v : adj[index])
+		{
+			if (!visited[v])
+			{
+				if (isCyclic(adj, visited, pathVisited, v, ans))
+					return true;
+			}
+			else if (pathVisited[v])
+				return true;
+		}
+		pathVisited[index] = false;
+		ans.push_back(index);
+		return false;
+	}
+
+public:
+	// Function to return list containing vertices in Topological order.
+	vector<int> topoSort(int V, vector<int> adj[])
+	{
+		// Topological sort DFS (inclusive of cycle detection (in a conventional way))
+		vector<bool> visited(V, false);
+		vector<bool> pathVisited(V, false);
+		vector<int> ans;
+		for (int i = 0; i < V; i++)
+		{
+			if (!visited[i] && isCyclic(adj, visited, pathVisited, i, ans))
+				return vector<int>();
+		}
+		reverse(ans.begin(), ans.end());
+		return ans;
 	}
 };
 
-// Solution 3
+// Solution 3 - solution 1, but with stack and some changes in isCycle function
 class Solution
 {
-    void topoSort(vector<int> adj[], vector<bool> &visited, int index, vector<int> &ans) {
-        visited[index] = true;
-        for(int v: adj[index]){
-            if(!visited[v])
-                topoSort(adj, visited, v, ans);
-        }
-        ans.push_back(index);
-    }
-	public:
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
+	bool isCycle(int index, stack<int> &stk, vector<int> adj[], vector<int> &visited)
 	{
-	    vector<bool> visited(V, false);
-	    vector<int> ans;
-	    for(int i=0; i<V; i++){
-	        if(!visited[i])
-	            topoSort(adj, visited, i, ans);
-	    }
-	    reverse(ans.begin(), ans.end());
-	    return ans;
+		visited[index] = 1;
+		for (int x : adj[index])
+		{
+			if (visited[x] == -1)
+				continue;
+			if (visited[x] == 1 || isCycle(x, stk, adj, visited))
+				return true;
+		}
+		visited[index] = -1;
+		stk.push(index);
+		return false;
+	}
+
+public:
+	vector<int> topoSort(int V, vector<int> adj[])
+	{
+		stack<int> stk;
+		vector<int> visited(V, 0);
+		for (int i = 0; i < V; i++)
+		{
+			if (!visited[i] && isCycle(i, stk, adj, visited))
+				return vector<int>();
+		}
+		vector<int> ans;
+		while (!stk.empty())
+		{
+			ans.push_back(stk.top());
+			stk.pop();
+		}
+
+		return ans;
 	}
 };
 
-// isCyclic should be checked before or during topoSort
-// For "during toposort" -> mark visited[i] with constants as per their traversal group -> check if cycle forms in the traversal of group
-// i.e. pass a unique value to visited[i] and compare visited[i] in else block (after  line 77)
+// Another way to check visited
+// -> mark all visited as 0 intially
+// -> mark visited[i] with constants for all nodes traversed when topoSort is called
+// -> check with this constant if visited[i] != 0
+
+// Wrong Solution 1 - cycle in graph is not checked, works only for DAG
+class WrongSolution
+{
+	stack<int> ansStack;
+
+public:
+	// Function to return list containing vertices in Topological order.
+	void dfsApproach(vector<int> adj[], int index, vector<bool> &visited)
+	{
+
+		visited[index] = true;
+		for (int i = 0; i < adj[index].size(); i++)
+		{
+			int child = adj[index][i];
+			if (!visited[child])
+				dfsApproach(adj, child, visited);
+		}
+		ansStack.push(index);
+	}
+	vector<int> topoSort(int V, vector<int> adj[])
+	{
+		vector<bool> visited(V, false);
+		for (int i = 0; i < V; i++)
+		{
+			if (!visited[i])
+				dfsApproach(adj, i, visited);
+		}
+		vector<int> ans;
+
+		while (!ansStack.empty())
+		{
+			ans.push_back(ansStack.top());
+			ansStack.pop();
+		}
+		return ans;
+	}
+};
+
+// Wrong Solution 2 - cycle in graph is not checked, works only for DAG
+class WrongSolution
+{
+	void topoSort(vector<int> adj[], vector<bool> &visited, int index, vector<int> &ans)
+	{
+		visited[index] = true;
+		for (int v : adj[index])
+		{
+			if (!visited[v])
+				topoSort(adj, visited, v, ans);
+		}
+		ans.push_back(index);
+	}
+
+public:
+	// Function to return list containing vertices in Topological order.
+	vector<int> topoSort(int V, vector<int> adj[])
+	{
+		vector<bool> visited(V, false);
+		vector<int> ans;
+		for (int i = 0; i < V; i++)
+		{
+			if (!visited[i])
+				topoSort(adj, visited, i, ans);
+		}
+		reverse(ans.begin(), ans.end());
+		return ans;
+	}
+};
